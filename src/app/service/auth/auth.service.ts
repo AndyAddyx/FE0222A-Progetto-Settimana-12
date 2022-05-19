924680c3b96e6f650d555ffc341e814af2e7ec75
Sergio Rinaudo <kaiohken1982@hotmail.com>
2022-05-19 23:35:14 +0200

    Auth service con qualche esempio e metodi vuoti

--- src/app/service/auth/auth.service.ts
+++ src/app/service/auth/auth.service.ts
@@ -1,9 +1,18 @@
+/**
+ * Angular
+ */
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject } from 'rxjs';
-import { tap } from 'rxjs';
-import { JwtHelperService } from '@auth0/angular-jwt';
 import { Router } from '@angular/router';
+
+/**
+ * RxJS
+ */
+import { BehaviorSubject, Observable } from 'rxjs';
+import { tap, map } from 'rxjs/operators'
+
+
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 export interface AuthData {
   accessToken: string;
@@ -12,6 +21,16 @@ export interface AuthData {
     id: number;
     name: string;
   };
+}
+
+export interface LoginRequest {
+  username: string;
+  password: string;
+}
+
+export interface LoginToken {
+  access_token: string;
+  refresh_token: string;
 }
 
 @Injectable({
@@ -26,6 +45,42 @@ export class AuthService {
   timeoutLogout:any
 
   constructor(private http: HttpClient, private router: Router) { }
+
+  // RICORDA è solo una traccia, non è completato, TODO!!!!!!
+  login(payload: LoginRequest): Observable<LoginToken> {
+    // const url = sprintf('%s%s', this.env.apiGateway, '/v1/auth/login')
+    const url = this.url
+    return this.http.post<LoginToken>(url, payload, {
+      // headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+      // observe: 'response'
+    }).pipe(
+      map((response: any) => {
+        const loginToken: LoginToken = {
+          access_token: response.body.access_token,
+          // expires_in: response.body.expires_in,
+          // token_type: response.body.token_type,
+          // scope: response.body.scope,
+          refresh_token: response.body.refresh_token,
+          // user_id: response.body.user_id
+
+          /**
+           * Sto porco bastardo ha smesso di funzionare
+           */
+          //user_id: response.headers.get('X-Api-User-Id')
+        }
+
+        // if(!loginToken.user_id) {
+        //   console.warn('Hey hey hey, userId is not exposed, check web server configuration')
+        // }
+
+        return loginToken
+      })
+    )
+  }
+
+  public logout() {}
+
+  public refresh() {}
 
   public isLoggedIn() {
     /**
